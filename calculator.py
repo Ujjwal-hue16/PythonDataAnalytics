703bd73a8aff8b35de00c43ed2d55dd2f4bba4a1
Ujjwal Gupta <ujjwalgupta1070@gmail.com>
2024-10-21 11:59:58 +0530

    Graph2

---
+++ calculator.py
@@ -0,0 +1,26 @@
+import streamlit as st
+
+st.title('calculator')
+st.markdown('welcome to my first streamlt app')
+
+c1, c2= st.columns(2)
+fnum= c1.number_input('enter first number', value=0)
+snum= c2.number_input('enter second number', value=0)
+options= ['add','subtract','multiply','divide']
+choice= st.radio('select operarion', options)
+
+button = st.button("calculate")
+
+
+if button:
+    if choice == "Add":
+         result = fnum + snum
+    if choice == "Subtract":
+        result = fnum - snum
+    if choice == "Multiply":
+         result = fnum*snum
+    if choice == "Divide":
+         result= fnum/snum
+    st.success(f'Result is {result}') 
+    
+
